1f73286668191a49be3e46dc18d32aca92bb2c04
xiaomi <xiaomi@qq.com>
2020-10-25 20:20:38 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,62 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <img class="img-test" src="~@/assets/img/src.png" />
+      <h3>vue background引用背景图</h3>
+      <div class="bg-test"></div>
+      <h3>vue中引用css样式(全局引用/局部引用)</h3>
+      <child-cpm></child-cpm>
+      <div>
+        <h3>vue中使用iconfont</h3>
+        <span class="iconfont icon-food-cookie"></span>
+        <span class="iconfont icon-food-pecan"></span>
+        <span class="iconfont icon-food-avocado"></span>
+        <span class="iconfont icon-food-pudding"></span>
+        <span class="iconfont icon-food-strawberry"></span>
+        <span class="iconfont icon-food-chips"></span>
+        <span class="iconfont icon-food-popcorn"></span>
+        <span class="iconfont con-food-bread"></span>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+import ChildCpm from "@/components/childcpm/ChildCpm";
+export default {
+  name: "",
+  components: {
+    ChildCpm
+  },
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  // 生命周期 - 创建完成（可以访问当前this实例,data,props）
+  created() {},
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, // 生命周期 - 创建之前
+  beforeMount() {}, // 生命周期 - 挂载之前
+  beforeUpdate() {}, // 生命周期 - 更新之前
+  updated() {}, // 生命周期 - 更新之后
+  beforeDestroy() {}, // 生命周期 - 销毁之前
+  destroyed() {}, // 生命周期 - 销毁完成
+  activated() {}, // 如果页面有keep-alive缓存功能，这个函数会触发
+  // 方法集合
+  methods: {}
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.bg-test {
+  width: 200px;
+  height: 200px;
+  background-image: url("~@/assets/img/bg.png");
+  background-size: 100%;
+  background-repeat: no-repeat;
 }
 </style>
